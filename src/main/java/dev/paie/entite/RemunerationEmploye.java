18cc77e11fad3ff32bd0b1ade32a4de2b2079c3f
Asia Trabelsi <assia.trabelsi93@gmail.com>
2017-12-20 14:20:43 +0100

    Mapping JPA

--- src/main/java/dev/paie/entite/RemunerationEmploye.java
+++ src/main/java/dev/paie/entite/RemunerationEmploye.java
@@ -9,60 +9,62 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.ManyToAny;
-
 @Entity
-@Table(name="renumerationEmploye")
+@Table(name = "renumerationEmploye")
 public class RemunerationEmploye {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	@Column(name="matricule")
+	@Column(name = "matricule")
 	private String matricule;
 	@ManyToOne
-	@JoinColumn(name="entreprise_id")
+	@JoinColumn(name = "entreprise_id")
 	private Entreprise entreprise;
 	@ManyToOne
-	@JoinColumn(name="profil_id")
+	@JoinColumn(name = "profil_id")
 	private ProfilRemuneration profilRemuneration;
 	@ManyToOne
-	@JoinColumn(name="grade_id")
+	@JoinColumn(name = "grade_id")
 	private Grade grade;
-	
+
 	public String getMatricule() {
 		return matricule;
 	}
+
 	public void setMatricule(String matricule) {
 		this.matricule = matricule;
 	}
-	
+
 	public Entreprise getEntreprise() {
 		return entreprise;
 	}
+
 	public void setEntreprise(Entreprise entreprise) {
 		this.entreprise = entreprise;
 	}
+
 	public ProfilRemuneration getProfilRemuneration() {
 		return profilRemuneration;
 	}
+
 	public void setProfilRemuneration(ProfilRemuneration profilRemuneration) {
 		this.profilRemuneration = profilRemuneration;
 	}
+
 	public Grade getGrade() {
 		return grade;
 	}
+
 	public void setGrade(Grade grade) {
 		this.grade = grade;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	
-	
-	
-	
 
 }
