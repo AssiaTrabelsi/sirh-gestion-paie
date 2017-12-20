5a8ea1b79d7d774212bb2f8f92d84924903f0466
Asia Trabelsi <assia.trabelsi93@gmail.com>
2017-12-20 09:21:30 +0100

    modification "ServiceConfig"

--- src/main/java/dev/paie/config/ServicesConfig.java
+++ src/main/java/dev/paie/config/ServicesConfig.java
@@ -3,12 +3,15 @@ package dev.paie.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-
 import dev.paie.spring.DataSourceMySQLConfig;
 import dev.paie.spring.JpaConfig;
+
+
 
 @Configuration
 @ComponentScan({"dev.paie.service","dev.paie.util"})
 @Import({JpaConfig.class,DataSourceMySQLConfig.class})
+
+
 public class ServicesConfig {
 }
